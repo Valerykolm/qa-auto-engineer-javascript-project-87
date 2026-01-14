94289e753f3eba536a1a6e01bd72da92ffd5b590
Vanessa <vanessa.ives@hubnero.com>
2026-01-14 08:48:34 +0200

    format type

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,6 +8,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .helpOption('-h, --help', 'output usage information');
 
 program.parse(process.argv);
