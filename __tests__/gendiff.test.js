075ad10a0cff9a7de7e9a585b859bf43d789506d
Valeria <valerycolm@gmail.com>
2026-02-11 18:16:50 +0200

    lint fix

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,6 +5,9 @@ import genDiff from '../src/index.js'
 const getFixturePath = filename =>
   path.resolve(process.cwd(), '__tests__/__fixtures__', filename)
 
+const readFile = filename =>
+  fs.readFileSync(getFixturePath(filename), 'utf-8')
+
 describe('genDiff flat files comparison', () => {
   test.each([
     ['file1.json', 'file2.json'],
@@ -18,4 +21,4 @@ describe('genDiff flat files comparison', () => {
 
     expect(genDiff(filePath1, filePath2)).toBe(expected)
   })
-});
+})
