925a811e367fc07111877705076e270e064bac55
Valeria <valerycolm@gmail.com>
2026-02-11 17:57:40 +0200

    added new logic in parse.js

--- src/parse.js
+++ src/parse.js
@@ -1,6 +1,12 @@
+import yaml from 'js-yaml'
+
 const parse = (data, format) => {
-  if ((format === 'json') || (format === 'yml') || (format === 'yaml')) {
+  if (format === 'json') {
     return JSON.parse(data)
+  }
+
+  if (format === 'yml' || format === 'yaml') {
+    return yaml.load(data)
   }
 
   throw new Error(`Unknown format: ${format}`)
